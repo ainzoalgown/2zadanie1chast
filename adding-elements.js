24258321af91c2691130512eae1d73d61912a394
sultan <malsagov.sa@inbox.ru>
2021-06-24 12:33:15 +0300

    2zadanie1chast

---
+++ adding-elements.js
@@ -0,0 +1,16 @@
+let a = []
+a.push("zizi")
+console.log(a)
+let b = []
+b.push("fesf", "faefae", "efaefefe")
+console.log(b)
+let c = [true, false]
+c.unshift(null)
+console.log(c)
+let d = [[], []]
+d[1].push("fsrfs", "rgsrgsr", "fsfsefe" )
+console.log(d)
+let e = ["html", "css"]
+e.push("js")
+e.unshift("git")
+console.log(e)
